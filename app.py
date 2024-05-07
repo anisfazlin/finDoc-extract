62073ef301203a4abc884c291c385600a1cdf8dd
Fazlin <51455715+anisfazlin@users.noreply.github.com>
2024-05-07 23:17:10 +0800

    Update app.py implement on streamlit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import pypdfium2 as pdfium
 import streamlit as st
 import multiprocessing
+from tempfile import NamedTemporaryFile
 import pandas as pd
 import json
 import requests
@@ -20,27 +21,82 @@ load_dotenv()
 open_api_key = os.getenv("OPENAI_API_KEY")
 llm = ChatOpenAI(temperature=0.4, model="gpt-3.5-turbo-0125")
 
-def classify_document(text):
-    tagging_prompt = ChatPromptTemplate.from_template(
-        """
-        Extract the desired information from the following passage.
+#CSS Style for Streamlit page
+#Remove top padding
+st.markdown('''<style>.css-1egvi7u {margin-top: -4rem;}</style>''',
+    unsafe_allow_html=True)
 
-        Only extract the properties mentioned in the 'Classification' function.
 
-        Passage:
-        {input}
-        """
+# 1. Convert PDF file into images via pypdfium2
+
+def convert_pdf_to_images(file_path, scale=300/72):
+
+    pdf_file = pdfium.PdfDocument(file_path)
+
+    page_indices = [i for i in range(len(pdf_file))]
+
+    renderer = pdf_file.render(
+        pdfium.PdfBitmap.to_pil,
+        page_indices=page_indices,
+        scale=scale
     )
 
-    class Classification(BaseModel):
-        category: str = Field(description="The category of the financial document.",
-                              enum=['expenses', 'donations', 'income'])
+    final_images = []
 
+    for i, image in zip(page_indices, renderer):
+
+        image_byte_array = BytesIO()
+        image.save(image_byte_array, format='jpeg', optimize=True)
+        image_byte_array = image_byte_array.getvalue()
+        final_images.append(dict({i: image_byte_array}))
+
+    return final_images
+
+# 2. Extract text from images via pytesseract
+
+def extract_text_from_img(list_dict_final_images):
+
+    image_list = [list(data.values())[0] for data in list_dict_final_images]
+    image_content = []
+
+    for index, image_bytes in enumerate(image_list):
+
+        image = Image.open(BytesIO(image_bytes))
+        raw_text = str(image_to_string(image))
+        image_content.append(raw_text)
+ 
+    return "\n".join(image_content)
+
+def extract_content(text: str):
+    images_list = convert_pdf_to_images(text)
+    text_with_pytesseract = extract_text_from_img(images_list)
+
+    return text_with_pytesseract
+
+# 3. Categorization and Template Mapping Chain
+
+class Classification(BaseModel):
+    category: str = Field(description="The category of the financial document.", 
+                          enum = ['expenses', 'donations', 'income'])
+
+def classify_document(text):
+    
+    tagging_prompt = PromptTemplate.from_template(
+    """
+    Extract the desired information from the following passage.
+
+    Only extract the properties mentioned in the 'Classification' function.
+
+    Passage:
+    {input}
+    """
+    )
+        
     llmTagging = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125").with_structured_output(
-        Classification)
+    Classification)
 
-    tagging_chain = tagging_prompt | llmTagging
-
+    tagging_chain = tagging_prompt | llmTagging    
+    
     return tagging_chain.invoke(input=text).category
 
 def generate_prompt_template(category: str):
@@ -52,14 +108,14 @@ def generate_prompt_template(category: str):
             "Amount": "amount of the expense",
             "Description": "description of the expense"
             """,
-
+            
         "donations": """
             "Date": "date of the donation",
             "Organization": "organization receiving the donation",
             "Amount": "amount of the donation",
             "Description": "description of the donation"
             """,
-
+            
         "income": """
             "Date": "date of the income",
             "Source": "source of the income",
@@ -67,23 +123,21 @@ def generate_prompt_template(category: str):
             "Description": "description of the income"
             """
     }
+
     # Retrieve the appropriate prompt template based on the category
     prompt_template = data_points.get(category.lower(), "Unknown category")
 
     return prompt_template
 
 def extract_structured_data(content: str, data_points):
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+
     template = """
     You are an expert financial admin people who will extract core information from financial documents
 
     {content}
 
-    Above is the content; please try to extract all data points from the content above 
-    and export in a JSON array format:
+    Above is the content; please try to extract all data points and return in a JSON array format
     {data_points}
-
-    Now please extract details from the content and return in a JSON array format.
     """
 
     prompt = PromptTemplate(
@@ -91,19 +145,86 @@ def extract_structured_data(content: str, data_points):
         template=template,
     )
 
-    chain = SequentialChain(
-        classify_document,
-        lambda category: generate_prompt_template(category),
-        llm
-    )
+    chain = LLMChain(llm=llm, prompt=prompt)
 
-    results = chain.run(content)
+    results = chain.run(content=content, data_points=data_points)
 
     return results
 
-# Example usage
-content_text = "This is a financial document describing an expense."
-data_points = "expenses"  # Specify the category for data extraction
+def process_document(text):
 
-extracted_details = extract_structured_data(content_text, data_points)
-print("Extracted Details:", extracted_details)
+    detected_category = classify_document(text)
+    print(detected_category)
+
+    try:
+        # Define the extraction prompt based on the detected category
+        template_prompt = generate_prompt_template(detected_category)
+        extracted_data = extract_structured_data(text, template_prompt)
+        
+        return extracted_data
+    except:
+        return "Detected category does not match the expected category."
+
+def get_data(category: str, content: str):
+    #Retrieve the appropriate prompt template based on the category
+    data_points = generate_prompt_template(category)
+    #Extract structured data
+    data = extract_structured_data(content, data_points)
+    return data
+
+
+def main():
+    st.markdown('Generate your personal ledger by uploading your financial documents - powered by Artificial Intelligence.')
+    st.title("FiscalFile Forge: Unleash your financial data")
+    
+    st.write('\n')  # add spacing
+
+    st.subheader('\nWhat is your email all about?\n')
+    with st.expander("SECTION - Upload Financial Documents", expanded=True):
+        uploaded_files = st.file_uploader("Choose an image financial file", accept_multiple_files=True)
+        
+        if uploaded_files:
+            results = []  # Initialize results list outside the loop
+
+            # Process each uploaded file
+            for uploaded_file in uploaded_files:
+                with NamedTemporaryFile(suffix='.csv') as f:
+                    f.write(uploaded_file.getbuffer())
+                    f.seek(0)  # Reset file pointer to beginning
+                    content = extract_content(f.name)
+                    
+                    # Classify document and extract structured data
+                    # category = classify_document(content)
+                    # data = process_document(content)
+                    classification_result = classify_document(content)
+                    category = classification_result
+                    
+                    categories = ["expenses", "donations", "income"]
+                    #DYnamically change displayed category based on classification
+                    selected_category = st.selectbox("Select a category to view the data", categories, index=categories.index(category))
+                    
+                    data_points_template = generate_prompt_template(selected_category)
+                    data = extract_structured_data(content, data_points_template)
+                    
+                    # Parse data and append to results
+                    json_data = json.loads(data)
+                    if isinstance(json_data, list):
+                        results.extend(json_data)  # Use extend() for lists
+                    else:
+                        results.append(json_data)  # Wrap the dict in a list
+
+            # Display results if any documents were processed
+            if results:
+                try:
+                    df = pd.DataFrame(results)
+                    st.subheader("Results")
+                    
+                    st.data_editor(df)
+
+                except Exception as e:
+                    st.error(f"An error occurred while creating the DataFrame: {e}")
+                    st.write(results)  # Print the data to see its content
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    main()
