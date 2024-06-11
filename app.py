008d4c78c206c806ff55e8b9ab6c5b274e45c472
Fazlin <51455715+anisfazlin@users.noreply.github.com>
2024-06-11 12:19:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import requests
 import os
 
 from langchain_core.prompts import PromptTemplate
-from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain_core.pydantic_v1 import BaseModel, Field
